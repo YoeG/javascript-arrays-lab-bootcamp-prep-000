7b4a1ae67ae5e58c62691059d3a9168f9d6d4e25
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-16 14:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,16 +8,12 @@ console.log(kittens[0]);
 function destructivelyAppendKitten (name)
 {kittens.push(name)}
 
-destructivelyAppendKitten("Yoe")
-console.log(kittens[3]);
 
 ///////////////////////////////////////////////////////////////////////////////
 
 function destructivelyPrependKitten (name)
 {kittens.unshift(name)}
-var aName = "Juan"
-destructivelyPrependKitten (aName)
-console.log(kittens[0])
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
