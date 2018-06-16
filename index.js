8e27db08a0c68f504a352dd99b405fd58cea03ae
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-16 13:56:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 console.log(kittens[0]);
 
+
+///////////////////////////////////////////////////////////////////////////////
 function destructivelyAppendKitten (name)
 {kittens.push(name)}
 
 destructivelyAppendKitten("Yoe")
 console.log(kittens[3]);
+
+///////////////////////////////////////////////////////////////////////////////
+
+function 
+
+
